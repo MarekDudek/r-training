e7d3379bce70ae21f19d9b36b2eb5dc482e648d9
Marek Dudek <marek.dudek@gmail.com>
2015-01-05 05:00:47 +0100

    Accessing matrices

--- code-school/matrices.R
+++ code-school/matrices.R
@@ -15,3 +15,23 @@ dim(plank) <- c(2, 4)
 dim(plank) <- c(4, 2)
 
 truth.table = matrix(c(T,T, F, F, T, F, T, F), 4, 2) # matrices can hold any type of value
+
+## Matrix access
+
+a <- matrix(1:6, 2, 3)
+assert(a[2, 3] == 6) # first row, then column
+
+a[1, 2] = 7 # changing value
+
+second.row = a[2,]    # accessing whole row
+first.column = a[,1]  # accessing whole colunn
+
+assert(second.row == c(2, 4, 6))
+assert(first.column == c(1, 2))
+
+# accessing multiple rows and columns
+
+b <- matrix(seq(1:15), 5, 3)
+
+second.and.fourth.row  <- b[c(2,4), ]
+first.and.third.column <- b[ ,c(1,3)]
