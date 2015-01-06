8edce7e27349fb421e7b6789403fae62b09d8a5e
Marek Dudek <marek.dudek@gmail.com>
2015-01-06 17:46:29 +0100

    Plots with ggplot2

--- code-school/real-world-data.R
+++ code-school/real-world-data.R
@@ -14,3 +14,16 @@ cor.test(countries$GDP, countries$Piracy)
 
 line <- lm(countries$Piracy ~ countries$GDP)
 abline(line)
+
+
+# better plots
+help(package='ggplot2')
+
+library(ggplot2)
+
+weights <- c(300, 200, 100, 250, 150)
+prices <- c(9000, 5000, 12000, 7500, 18000)
+chests <- c('gold', 'silver', 'gems', 'gold', 'gems')
+types <- factor(chests)
+
+qplot(weights, prices, color = types)
