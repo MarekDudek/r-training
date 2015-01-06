c0e959d61ddcc64aea0290db989643d92c587445
Marek Dudek <marek.dudek@gmail.com>
2015-01-07 00:17:22 +0100

    Data type finished

--- coursera/data-types.R
+++ coursera/data-types.R
@@ -59,6 +59,10 @@ assert(is.null(null))
 ## vectors can only hold object of the same class
 ## except lists
 
+list <- list(1, "a", TRUE, 1+4i)
+assert(list[1] == 1)
+assert(list[2] == 'a')
+
 # Attributes
 ## names, dimnames
 ## dimensions
@@ -86,3 +90,64 @@ assert(class(x) == 'logical')
 
 x <- as.character(x)
 assert(class(x) == 'character')
+
+# Matrices
+m1 <- matrix(nrow = 2, ncol = 3)
+assert(dim(m1) == c(2, 3))
+
+m2 <- 1:10
+dim(m2) <- c(2,5)
+
+x <- 1:3
+y <- 10:12
+
+m3 <- cbind(x, y)
+assert(dim(m3) == c(3, 2))
+
+m4 <- rbind(x, y)
+assert(dim(m4) == c(2, 3))
+
+# Factors - special kind of vector, self-describing
+## ordered
+## unorders
+
+# have level attribute
+f <- factor(c("yes", "yes", "no", "yes", "no")) 
+
+table(f)  # frequency count
+unclass(f)
+
+f <- factor(c("yes", "yes", "no", "yes", "no"), levels=c('yes', 'no')) 
+
+# Missing values
+x <- c(1, 2, NA, 10, 3)
+x.is.na = is.na(x)
+
+## Data frames - tabular data
+# special type of list
+# every 
+# every element is column of data
+# attribute row.names
+
+df <- data.frame(foo = 1:4, bar = c(T, T, F, F))
+assert(nrow(df) == 4)
+assert(ncol(df) == 2)
+
+m <- data.matrix(df)
+
+# Names - for every object
+x <- 1:3
+assert(is.null(names(x)))
+
+names(x) <- c("foo", "bar", "norf")
+assert(names(x) == c("foo", "bar", "norf"))
+
+x <- list(a = 1, b = 2, c = 3)
+assert(names(x) == c("a", "b", "c"))
+
+m <- matrix(1:4, nrow = 2, ncol = 2)
+dimnames(m) <- list(c("a", "b"), c("c", "d"))
+
+d <- dimnames(m)
+assert(d[[1]] == c("a", "b"))
+assert(d[[2]] == c("c", "d"))
