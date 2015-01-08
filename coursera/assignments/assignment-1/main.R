44123286ddaa3c66608d57eb7db0010af4a7cc59
Marek Dudek <marek.dudek@gmail.com>
2015-01-09 00:56:08 +0100

    Function signature and code running examples

---
+++ coursera/assignments/assignment-1/main.R
@@ -0,0 +1,16 @@
+## Main file, running tests
+
+setwd("/home/marek/Devel/data-science/r-training/coursera/assignments/assignment-1")
+source("pollutantmean.R")
+
+
+## Tests
+
+pm1 <- pollutantmean("specdata", "sulfate", 1:10)
+print.("pm1 ==", pm1)
+
+pm2 <- pollutantmean("specdata", "nitrate", 70:72)
+print.("pm2 ==", pm2)
+
+pm3 <- pollutantmean("specdata", "nitrate", 23)
+print.("pm3 ==", pm3)
