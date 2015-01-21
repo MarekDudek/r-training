bd10d6b78b073834b32716aca3728af23457d2a4
Marek Dudek <marek.dudek@gmail.com>
2015-01-21 15:32:56 +0100

    All test cases pass

--- coursera/assignments/assignment-3/rankhospital.R
+++ coursera/assignments/assignment-3/rankhospital.R
@@ -12,14 +12,24 @@ names(column.names) <- c('heart attack', 'heart failure', 'pneumonia')
 rankhospital <- function(state, outcome, num = 'best') {
 
     data <- read.csv('outcome-of-care-measures.csv', colClasses = 'character')
+    
     data.in.state <- data[data$State == state, ]
     
     column <- column.names[[outcome]]
-    ordered <- data.in.state[order(as.numeric(data.in.state[, column]), data.in.state$Hospital.Name), ]
+    numbers <- suppressWarnings( as.numeric(data.in.state[, column]) )
+    
+    ordered <- data.in.state[order(numbers, data.in.state$Hospital.Name), ]
+    
+    if (num == 'best') {
+        num <- 1
+    } else if (num == 'worst') {
+        available <- numbers[!is.na(numbers)]
+        num <- length(available)
+    }
     
     hospital <- ordered[num, ]
-    name <- hospital$Hospital.Name
     
+    name <- hospital$Hospital.Name
     name
 }
 
@@ -33,6 +43,35 @@ hospital <- rankhospital('TX', 'heart failure', 4)
 ## Then
 assert( hospital == 'DETAR HOSPITAL NAVARRO' )
 
+#########################################################################################
+## Best for heart failure in Texas
+#########################################################################################
+
+## When
+hospital <- rankhospital('TX', 'heart failure', 'best')
+
+## Then
+assert( hospital == 'FORT DUNCAN MEDICAL CENTER' )
+
+#########################################################################################
+## Worst for heart attack in Maryland
+#########################################################################################
+
+## When
+hospital <- rankhospital('MD', 'heart attack', 'worst')
+
+## Then
+assert( hospital == 'HARFORD MEMORIAL HOSPITAL' )
+
+#########################################################################################
+## Too low rank for heart attack in Maryland
+#########################################################################################
+
+## When
+hospital <- rankhospital('MD', 'heart attack', 5000)
+
+## Then
+assert( is.na(hospital) )
 
 #########################################################################################
 #########################################################################################
